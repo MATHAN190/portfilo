f1be80870a79700bea7a4665bfc7e6b9be2cab59
MATHAN KUMAR <146416514+MATHAN190@users.noreply.github.com>
2025-08-28 14:44:38 +0530

    project.jsx
    update Ecom Website

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -8,15 +8,15 @@ export default function Project(){
                <div className=" flex flex-col md:flex-row  gap-7 justify-around  items-center p-5 ">
                       
                        <div className="  bg-indigo-500   rounded-2xl text-center pointer w-1/2 hover:shadow-2xl p-2 " style={{width:250, height:250}} >
-                            <h1 className="text-xl text-white font-bold" >Shoe_Cart Website</h1>
-                            <p className="font-sans p-2 mt-5"> Shoe cart Website using front-end development using html,css,js,react</p>
-                             <button className="bg-white w-20 h-10 rounded-2xl mt-10 text-indigo-500 font-bold  "><a href="https://shoe-cart-63c18.web.app/" className="">View</a></button>
+                            <h1 className="text-xl text-white font-bold" >E-Com Website</h1>
+                            <p className="font-sans p-2 mt-5"> Vonto E-com website create using React and Tailwind css</p>
+                             <button className="bg-white w-20 h-10 rounded-2xl mt-10 text-indigo-500 font-bold  "><a href="https://madhanvonto.netlify.app/" className="">View</a></button>
                        </div>
 
                         <div className=" bg-indigo-500 hover:shadow-2xl  rounded-2xl text-center pointer w-1/2 p-2 " style={{width:250, height:250}} >
-                            <h1 className="text-xl text-white font-bold" >Shoe_Cart App</h1>
-                            <p className="font-sans p-2 mt-5"> Shoe cart app create using flutter connecting back-end and database using FireBase </p>
-                            <button className="bg-white w-20 h-10 rounded-2xl mt-10 text-indigo-500 font-bold   "><a href="https://www.linkedin.com/posts/madhan-kumar-k-ma190_created-music-together-app-using-flutter-activity-7342425971813175297-Nguf?utm_source=social_share_send&utm_medium=android_app&rcm=ACoAAECOk54BLeXAqEqoMxJ63_7f8FzX3GAX_v8&utm_campaign=share_via" className="">View</a></button>
+                            <h1 className="text-xl text-white font-bold" >Shoe_Cart Website</h1>
+                            <p className="font-sans p-2 mt-5">  Shoe cart Website using front-end development using html,css,js,react </p>
+                            <button className="bg-white w-20 h-10 rounded-2xl mt-10 text-indigo-500 font-bold   "><a href="https://shoe-cart-63c18.web.app/" className="">View</a></button>
                        </div>
 
                        <div className=" bg-indigo-500   hover:shadow-2xl  p-2 rounded-2xl text-center pointer w-1/2 " style={{width:250, height:250}} >
